0c8a4171ed3f8f382c7583b2ee1ba30f58dd4891
Simrat Singh Chhabra <simratsinghchhabra@gmail.com>
2023-12-04 22:44:05 -0800

    add bowling line up generation

--- data_cleaning/team_prep.py
+++ data_cleaning/team_prep.py
@@ -15,8 +15,30 @@ from odi_data_explore import (
     FILE_PATH
 )
 
+
+def get_bowling_line_up(team_name, file_data):
+    over_counts = {}
+    bowling_line_up = []
+    player_registry = file_data["info"]["registry"]["people"]
+    for inning in file_data['innings']:
+        if inning['team'] == team_name:
+            continue
+        for over in inning['overs']:
+            bowler_name = over['deliveries'][0]['bowler']
+            bowling_line_up.append((player_registry[bowler_name], bowler_name))
+            if bowler_name in over_counts:
+                over_counts[bowler_name] += 1
+            else:
+                over_counts[bowler_name] = 1
+        
+    # handle remaining overs 
+    while len(bowling_line_up) < 50:        
+        bowler_name = [x for x in sorted(over_counts.items(), key=lambda item: item[1], reverse=True) if x[1] < 10 and x[0]!=bowling_line_up[-1][1]][0][0]
+        bowling_line_up.append((player_registry[bowler_name], bowler_name))
+        over_counts[bowler_name] += 1
+    return bowling_line_up
+
 def main():
-    team1 = {}
     team={}
     if not glob.glob(os.path.join(FILE_PATH, '*.json')):
         raise "Check file path"
@@ -24,20 +46,23 @@ def main():
         file_data =  process_file(filename)   
         match_id = get_match_id(filename)
         match_date = file_data["info"]["dates"][0]
-        if match_date == '2023-09-27':
+        if match_date == '2023-11-15':
             for team_name in file_data["info"]["players"].keys():
-                player = {}
                 if team_name in team and file_data["info"]["gender"]=="female":
                     continue
                 else:
                     team[team_name] = {}
+                    team[team_name]['batting_line_up'] = {}
+                    # get batting line up
                     for player_name in file_data["info"]["players"][team_name]:
-                        team[team_name][file_data["info"]["registry"]["people"][player_name]] = player_name
+                        team[team_name]['batting_line_up'][file_data["info"]["registry"]["people"][player_name]] = player_name
+                    # get bowling line up 
+                    team[team_name]['bowling_line_up'] = get_bowling_line_up(team_name, file_data)
 
     with open('teams.pkl', 'wb') as f:
         pickle.dump(team, f)
 
-    print(team["India"])
+    print(team)
 
 if __name__ == "__main__":
     main()
